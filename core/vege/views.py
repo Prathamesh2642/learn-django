f451223f81e4427143f753d1660430b46757143f
prathamesh <prathameshpatil2604@gmail.com>
2024-02-09 15:07:50 +0530

    completed video 1 of receipe

--- core/vege/views.py
+++ core/vege/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
+from .models import *
 # Create your views here.
 
 def receipes(request):
+    if request.method=='POST':
+        #obtain data into backend 
+        data=request.POST
+        receipe_image=request.FILES.get("receipe_image")
+        receipe_name=data.get('receipe_name')
+        receipe_description=data.get('receipe_description')
+
+        # store it in db
+        Receipe.objects.create(receipe_name=receipe_name,
+                                receipe_description=receipe_description,
+                                 receipe_image=receipe_image)
+        
+        return redirect('/receipes')
+
+        # print(receipe_name,receipe_description,receipe_image)
     return render(request,"receipes.html")
